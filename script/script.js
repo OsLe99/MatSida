1dc97d4a4035d43e9c1a074dd5732737ccb98c7a
emil94berg <emil94berg@gmail.com>
2025-04-10 11:07:54 +0200

    sök funktion fungerar

--- script/script.js
+++ script/script.js
@@ -1,17 +1,19 @@
 const url = "https://dataportal.livsmedelsverket.se/livsmedel/api/v1/livsmedel?offset=0&limit=2532&sprak=1";
 const mat = document.getElementById("mat");
+let allFood = [];
 
 
-fetch(url)
-    .then(function (response) {
-        return response.json()
-    }).then(function (data) {
-        console.log(data);
-        //Här ska vi bygga html kod som hämtar sin data från API:et
-        //Hämtar livsmedel och skriver ut dem i en lista
-        data.livsmedel.map(function (food) {
-            console.log(food);
-            let card = document.createElement("div");
+
+function searchFood(foodSearch) 
+{
+    const textSearch = foodSearch.toLowerCase();
+    mat.innerHTML = ""; // Rensa tidigare resultat
+    const filteredData = allFood.filter(function (food) {
+        return food.namn.toLowerCase().includes(textSearch);
+    });
+
+    filteredData.map(function (food) {
+        let card = document.createElement("div");
             card.setAttribute("class", "card");
 
             let title = document.createElement("h3");
@@ -24,11 +26,16 @@ fetch(url)
             card.appendChild(number);
             card.appendChild(title);
             mat.appendChild(card);
-        })
-        
-        
-
-        
-    }).catch(function (error) {
-        console.log("Error: " + error);
     })
+}
+
+
+fetch(url)
+    .then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        allFood = data.livsmedel;
+        searchFood(""); // Visa alla livsmedel vid första inläsning
+        }).catch(function (error) {
+            console.error("Error fetching data:", error);
+        });
